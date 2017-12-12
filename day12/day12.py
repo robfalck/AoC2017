0443825ee91df3f27cfd75acf04ad0e34ef0397b
Rob Falck <rfalck@nasa.gov>
2017-12-12 07:50:41 -0500

    day12 complete

---
+++ day12/day12.py
@@ -0,0 +1,49 @@
+from __future__ import print_function, division, absolute_import
+
+import networkx as nx
+
+
+def solve(puzzle_input):
+    G = nx.Graph()
+
+    nodes = range(len(puzzle_input))
+
+    G.add_nodes_from(nodes)
+
+    for line in puzzle_input:
+        source, targets = line.strip().split('<->')
+        targets = [int(target.strip()) for target in targets.split(',') if target.strip() != '']
+        G.add_edges_from([(int(source), target) for target in targets])
+
+    # Part 1
+    nodes_connected_to_zero = [i for i in nodes if nx.has_path(G, 0, i)]
+    print('Number of nodes connected to 0:', len(nodes_connected_to_zero))
+
+    # Part 2
+    groups = {0: nodes_connected_to_zero}
+    grouped_nodes = set(nodes_connected_to_zero)
+
+    for i in nodes:
+        if i not in grouped_nodes:
+            # Not found, create a new group
+            nodes_connected_to_i = [node for node in nodes if nx.has_path(G, node, i)]
+            groups[i] = [i] + nodes_connected_to_i
+            grouped_nodes |= set(groups[i])
+
+    print('Number of groups', len(groups))
+
+
+
+if __name__ == '__main__':
+
+    with open('test_input.txt', 'r') as f:
+        puzzle_input = f.readlines()
+
+    solve(puzzle_input)
+
+    print()
+
+    with open('input.txt', 'r') as f:
+        puzzle_input = f.readlines()
+
+    solve(puzzle_input)
